c5f02e46bb5ac70325c473f0ec8c69d2e481fd3c
savanitaliya11 <40891740+savanitaliya11@users.noreply.github.com>
2022-03-10 17:02:13 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
+///Hello World!!!!!
   runApp(MyApp());
 }
 
